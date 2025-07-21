8dadf944cdf949fb63f3bb7de8934195999a2679
prbasyal <prbasyal@amd.com>
2025-07-21 16:31:35 -0400

    Date updated for GA

--- docs/conf.py
+++ docs/conf.py
@@ -90,7 +90,7 @@ all_article_info_author = ""
 
 # pages with specific settings
 article_pages = [
-    {"file": "about/release-notes", "os": ["linux"], "date": "2025-06-26"},
+    {"file": "about/release-notes", "os": ["linux"], "date": "2025-07-21"},
     {"file": "release/changelog", "os": ["linux"],},
     {"file": "compatibility/compatibility-matrix", "os": ["linux"]},
     {"file": "compatibility/ml-compatibility/pytorch-compatibility", "os": ["linux"]},
