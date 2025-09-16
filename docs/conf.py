9fc6c65dfe7b062d9fddf49f1a9e11ce7ca303f6
Pratik Basyal <prbasyal@amd.com>
2025-09-16 08:07:40 -0400

    700 reset link [Develop] (#5325)
    * TOC link update and manifest removed
    
    * Link reset
    
    * Changelog synced

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ all_article_info_author = ""
 
 # pages with specific settings
 article_pages = [
-    {"file": "about/release-notes", "os": ["linux"], "date": "2025-08-28"},
+    {"file": "about/release-notes", "os": ["linux"], "date": "2025-09-16"},
     {"file": "release/changelog", "os": ["linux"],},
     {"file": "compatibility/compatibility-matrix", "os": ["linux"]},
     {"file": "compatibility/ml-compatibility/pytorch-compatibility", "os": ["linux"]},
